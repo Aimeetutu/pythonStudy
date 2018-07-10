737aaf4cec59903cdd1f22b050477a9b459b706a
Aimeetutu <1069923094@qq.com>
2018-07-10 16:57:21 +0800

    hahhaa

--- python2.py
+++ python2.py
@@ -11,13 +11,17 @@ msg["Subject"] = "send email"
 msg["From"]    = _user
 msg["To"]      = _to
 
-    try:
-        s = smtplib.SMTP_SSL("smtp.qq.com", 465)
-        s.login(_user, _pwd)
-        s.sendmail(_user, _to, msg.as_string())
-        s.quit()
-        print "Success!"
-    except smtplib.SMTPException,e:
-        print "Falied,%s"%e
+try:
+    s = smtplib.SMTP_SSL("smtp.qq.com", 465)
+    s.login(_user, _pwd)
+    s.sendmail(_user, _to, msg.as_string())
+    s.quit()
+    print ("Success!")
+except smtplib.SMTPException,e:
+    print("Falied,%s" % e)
+
+
+
+print("hahhahah")
 
 
